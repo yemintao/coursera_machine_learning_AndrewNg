92ce2ed9f89a2a272e868114a93600b95146f18c
mintao <ye.mintao@gmail.com>
2015-09-12 18:22:20 +0100

    Signed-off-by: mintao <ye.mintao@gmail.com>

---
+++ machine-learning-ex5/ex5/trainLinearReg.m
@@ -0,0 +1,27 @@
+function [theta] = trainLinearReg(X, y, lambda, varargin)
+%TRAINLINEARREG Trains linear regression given a dataset (X, y) and a
+%regularization parameter lambda
+%   [theta] = TRAINLINEARREG (X, y, lambda) trains linear regression using
+%   the dataset (X, y) and regularization parameter lambda. Returns the
+%   trained parameters theta.
+%
+
+% Initialize Theta
+if nargin == 1
+    initial_theta = varargin(1);
+else
+    initial_theta = zeros(size(X, 2), 1); 
+end
+
+
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(t) linearRegCostFunction(X, y, t, lambda);
+
+% Now, costFunction is a function that takes in only one argument
+options = optimset('MaxIter', 200, 'GradObj', 'on');
+
+% Minimize using fmincg
+theta = fmincg(costFunction, initial_theta, options);
+
+end
